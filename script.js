0016b8fe575470c695366ece5ad6e7be367b4fc9
surekhaazureacademy <sgalla1029@gmail.com>
2026-08-05 13:10:09 -0500

    Add carousel slide navigation and autoplay functionality

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-
+function prevSlide() {
+  const carousel = document.getElementById('videoCarousel');
+  carousel.scrollLeft -= carousel.offsetWidth;
+}
+ 
+function nextSlide() {
+  const carousel = document.getElementById('videoCarousel');
+  carousel.scrollLeft += carousel.offsetWidth;
+}
+function autoplaySlides() {
+  const carousel = document.getElementById('videoCarousel');
+  setInterval(() => {
+    carousel.scrollLeft += carousel.offsetWidth;
+    if (carousel.scrollLeft >= carousel.scrollWidth - carousel.offsetWidth) {
+      carousel.scrollLeft = 0;
+    }
+  }, 5000);
+}
+ 
+window.onload = autoplaySlides;
